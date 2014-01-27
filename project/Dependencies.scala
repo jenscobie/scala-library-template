2e74b555781feee2401eb5afcf3e6bf39668595e
Jen Scobie <jenscobie@gmail.com>
2014-01-27 00:10:30 -0800

    Added specs2 as a default dependency and a hello world spec example

---
+++ project/Dependencies.scala
@@ -0,0 +1,6 @@
+import sbt._
+
+object Dependencies {
+
+  val specs2 = "org.specs2" %% "specs2" % "2.3.7" % "test"
+}
--- project/OurBuild.scala
+++ project/OurBuild.scala
@@ -3,7 +3,19 @@ import Keys._
 
 object OurBuild extends Build {
 
+  val defaultSettings = Defaults.defaultSettings ++ Seq(
+    name := "sbt-project-template",
+    version := "1.0",
+    scalaVersion := "2.10.3",
+    libraryDependencies ++= Seq(Dependencies.specs2)
+  )
+
   lazy val root = Project(id = "sbt-project-template",
     base = file("."),
     settings = Project.defaultSettings)
+  .aggregate(someLibrary)
+
+  lazy val someLibrary = Project(id = "some-library",
+    base = file("some-library"),
+    settings = defaultSettings)
 }
