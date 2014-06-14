322fd6573fe3b856e72b63c9d7dcab49267991d7
jen <jen@loyal3.com>
2014-06-14 12:57:07 -0700

    Updated sbt-idea plugin to 1.6.0

--- project/project/Plugins.scala
+++ project/project/Plugins.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Plugins extends Build {
 
-  lazy val defaultPlugins = Seq(addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.5.2"))
+  lazy val defaultPlugins = Seq(addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.6.0"))
 
   lazy val plugins = Project("plugins",
     file("."),
